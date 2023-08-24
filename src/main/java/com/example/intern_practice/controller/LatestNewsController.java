da065f94347468d053a9667db323e8f88ce4bdc7
Jyun <taiwanman1212@gmail.com>
2023-08-24 11:37:24 +0800

    build news-list page

--- src/main/java/com/example/intern_practice/controller/LatestNewsController.java
+++ src/main/java/com/example/intern_practice/controller/LatestNewsController.java
@@ -1,51 +1,88 @@
 package com.example.intern_practice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.intern_practice.service.ifs.LatestNewsService;
 import com.example.intern_practice.vo.AddNewsRequest;
 import com.example.intern_practice.vo.ChangeNewsRequest;
-import com.example.intern_practice.vo.Response;
 import com.example.intern_practice.vo.ReviseNewsRequest;
 import com.example.intern_practice.vo.ShowNewsRequest;
 import com.example.intern_practice.vo.ShowNewsResponse;
 
-@RestController
+@Controller
 public class LatestNewsController {
-	
+
 	@Autowired
 	private LatestNewsService latestNewsService;
-	
-	@GetMapping(value = "show_opened_news")
-	public ShowNewsResponse showOpenedNews() {
+
+//	@GetMapping(value = "show_opened_news")
+//	public ShowNewsResponse showOpenedNews() {
+//		ShowNewsRequest request = new ShowNewsRequest();
+//		request.setReveal(true);
+//		return latestNewsService.showNews(request);
+//	}
+	@GetMapping(value = "/news_list_open")
+	public String showOpenedNews(Model model) {
 		ShowNewsRequest request = new ShowNewsRequest();
 		request.setReveal(true);
-		return latestNewsService.showNews(request);
-	}
-	
-	@GetMapping(value = "show_closed_news")
-	public ShowNewsResponse showClosedNews() {
-		ShowNewsRequest request = new ShowNewsRequest();
-		request.setReveal(false);
-		return latestNewsService.showNews(request);
-	}
-	
-	@PostMapping(value = "add_news")
-	public Response addNews(@RequestBody AddNewsRequest request) {
-		return latestNewsService.addNews(request);
+		ShowNewsResponse response = latestNewsService.showNews(request);
+		model.addAttribute("newsList", response.getNewsList());
+		return "news-list"; // 對應到模板名稱
 	}
 
-	@PostMapping(value = "revise_news")
-	public Response reviseNews(@RequestBody ReviseNewsRequest request) {
-		return latestNewsService.reviseNews(request);
+//	@GetMapping(value = "show_closed_news")
+//	public ShowNewsResponse showClosedNews() {
+//		ShowNewsRequest request = new ShowNewsRequest();
+//		request.setReveal(false);
+//		return latestNewsService.showNews(request);
+//	}
+	@GetMapping(value = "/news_list_close")
+	public String showClosedNews(Model model) {
+		ShowNewsRequest request = new ShowNewsRequest();
+		request.setReveal(false);
+		ShowNewsResponse response = latestNewsService.showNews(request);
+		model.addAttribute("newsList", response.getNewsList());
+		return "news-list"; // 對應到模板名稱
 	}
-	
-	@PostMapping(value = "change_news_status")
-	public Response changeNewsStatus(ChangeNewsRequest request) {
-		return latestNewsService.changeNewsStatus(request);
+
+//	@PostMapping(value = "add_news")
+//	public Response addNews(@RequestBody AddNewsRequest request) {
+//		return latestNewsService.addNews(request);
+//	}
+	@PostMapping(value = "/add_news")
+	public String addNews(@ModelAttribute AddNewsRequest request) {
+		latestNewsService.addNews(request);
+		return "redirect:/news_list_open"; // 重定向到列表頁面
 	}
+
+//	@PostMapping(value = "revise_news")
+//	public Response reviseNews(@RequestBody ReviseNewsRequest request) {
+//		return latestNewsService.reviseNews(request);
+//	}
+	@PostMapping(value = "/revise_news")
+	public String reviseNews(@ModelAttribute ReviseNewsRequest request) {
+		latestNewsService.reviseNews(request);
+		return "redirect:/news_list_open"; // 重定向到列表頁面
+	}
+
+//	@PostMapping(value = "change_news_status")
+//	public Response changeNewsStatus(ChangeNewsRequest request) {
+//		return latestNewsService.changeNewsStatus(request);
+//	}
+	@PostMapping(value = "/change_news_status")
+	public String changeNewsStatus(@ModelAttribute ChangeNewsRequest request) {
+		latestNewsService.changeNewsStatus(request);
+		return "redirect:/news_list_open"; // 重定向到列表頁面
+	}
+
+	@GetMapping(value = "turn_page")
+	public String turnPage() {
+		return "add-news";
+	}
+
 }
