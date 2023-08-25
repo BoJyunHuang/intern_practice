e2de442e11072606eea6595fca98249ccdab711a
Jyun <taiwanman1212@gmail.com>
2023-08-25 18:45:20 +0800

    plus japanese notes

--- src/main/java/com/example/intern_practice/controller/LatestNewsController.java
+++ src/main/java/com/example/intern_practice/controller/LatestNewsController.java
@@ -30,64 +30,143 @@ public class LatestNewsController {
 	@Autowired
 	private LatestNewsService latestNewsService;
 
+	/**
+	 * 開かれたニュースを表示するためのメソッドです。
+	 * 
+	 * @param model モデルオブジェクト
+	 * @return ニュースリストページのテンプレート名
+	 */
 	@GetMapping("/news_list_open")
 	public String showOpenedNews(Model model) {
+		// ニュースを表示するためのリクエストオブジェクトを作成し、表示を設定します
 		ShowNewsRequest request = new ShowNewsRequest();
 		request.setReveal(true);
+		// ニュースの表示リクエストをサービスに送信し、レスポンスを受け取ります
 		ShowNewsResponse response = latestNewsService.showNews(request);
+		// モデルに表示するニュースリストを追加します
 		model.addAttribute("newsList", response.getNewsList());
+		// ニュースリストの表示画面に遷移します
 		return "news-list";
 	}
 
+	/**
+	 * 閉じられたニュースを表示するためのメソッドです。
+	 * 
+	 * @param model モデルオブジェクト
+	 * @return ニュースリストページのテンプレート名
+	 */
 	@GetMapping("/news_list_close")
 	public String showClosedNews(Model model) {
+		// ニュースを表示するためのリクエストオブジェクトを作成し、表示を設定します
 		ShowNewsRequest request = new ShowNewsRequest();
 		request.setReveal(false);
+		// ニュースの表示リクエストをサービスに送信し、レスポンスを受け取ります
 		ShowNewsResponse response = latestNewsService.showNews(request);
+		// モデルに表示するニュースリストを追加します
 		model.addAttribute("newsList", response.getNewsList());
+		// ニュースリストの表示画面に遷移します
 		return "news-list";
 	}
 		
+	/**
+	 * ニュース追加ページを表示するためのメソッドです。
+	 * 
+	 * @param model モデルオブジェクト
+	 * @return ニュース追加ページのテンプレート名
+	 */
 	@GetMapping("/add_news")
 	public String toAddPage(Model model) {
+		// モデルにニュースリクエストオブジェクトと新規作成フラグをセットし、共通メソッドを呼び出して表示します
 	    return showNewsForm(model, new AddNewsRequest(), true);
 	}
 
+	/**
+	 * ニュースを追加するためのメソッドです。
+	 * 
+	 * @param request 追加リクエストオブジェクト
+	 * @param model モデルオブジェクト
+	 * @return ニュース一覧ページのリダイレクトテンプレート名
+	 */
 	@PostMapping("/add_news")
 	public String addNews(@ModelAttribute("news") AddNewsRequest request, Model model) {
+		// ニュースを追加するためのサービスメソッドを呼び出し、結果を処理するための共通メソッドを呼び出します
 	    Response res = latestNewsService.addNews(request);
 	    return processResponse(res, model, true);
 	}
 
+	/**
+	 * ニュース修正ページを表示するためのメソッドです。
+	 * 
+	 * @param newsId ニュースID
+	 * @param model モデルオブジェクト
+	 * @return ニュース修正ページのテンプレート名
+	 */
 	@GetMapping("/revise_news/{newsId}")
 	public String showReviseNewsForm(@PathVariable Integer newsId, Model model) {
+		// ニュースIDを用いてデータベースからニュースを取得し、共通メソッドを呼び出して表示します
 	    Optional<LatestNews> news = latestNewsDao.findById(newsId);
 	    return showNewsForm(model, news.get(), false);
 	}
 
+	/**
+	 * ニュースを修正するためのメソッドです。
+	 * 
+	 * @param request 修正リクエストオブジェクト
+	 * @param model モデルオブジェクト
+	 * @return ニュース一覧ページのリダイレクトテンプレート名
+	 */
 	@PostMapping("/revise_news")
 	public String reviseNews(@ModelAttribute("news") ReviseNewsRequest request, Model model) {
+		// ニュースを修正するためのサービスメソッドを呼び出し、結果を処理するための共通メソッドを呼び出します
 	    Response res = latestNewsService.reviseNews(request);
 	    return processResponse(res, model, false);
 	}
 
+	/**
+	 * ニュースの表示状態を変更するためのメソッドです。
+	 * 
+	 * @param request 変更リクエストオブジェクト
+	 * @param model モデルオブジェクト
+	 * @return ニュース一覧ページのリダイレクトテンプレート名
+	 */
 	@PostMapping("/change_news_status")
 	public String changeNewsStatus(@ModelAttribute ChangeNewsRequest request, Model model) {
+		// ニュースの表示状態を変更するためのサービスメソッドを呼び出します
 		Response res = latestNewsService.changeNewsStatus(request);
+		// レスポンスのメッセージをモデルにセットして、ニュース一覧ページにリダイレクトします
 		model.addAttribute("errorMessage", res.getMessage());
 		return "redirect:/news_list_open";
 	}
 	
+	/**
+	 * ニュースフォームを表示するためのメソッドです。
+	 * 
+	 * @param model モデルオブジェクト
+	 * @param news ニュースオブジェクト
+	 * @param isNew 新規作成フラグ（true: 新規作成モード, false: 修正モード）
+	 * @return ニュースフォームのテンプレート名
+	 */
 	private String showNewsForm(Model model, Object news, boolean isNew) {
+		// ニュースオブジェクトと新規作成フラグをモデルにセットしてニュースフォームのテンプレート名を返します
 		model.addAttribute("news", news);
 		model.addAttribute("isNew", isNew);
 		return "add-news";
 	}
 	
+	/**
+	 * サービスメソッドの結果を処理するための共通メソッドです。
+	 * 
+	 * @param res レスポンスオブジェクト
+	 * @param model モデルオブジェクト
+	 * @param isNew 新規作成フラグ（true: 新規作成モード, false: 修正モード）
+	 * @return レスポンスに基づいた遷移先テンプレート名
+	 */
 	private String processResponse(Response res, Model model, boolean isNew) {
 		if (res.getMessage().equals(RtnCode.SUCCESS.getMessage())) {
+			// 成功時はリダイレクトしてニュース一覧を表示します
 			return "redirect:/news_list_open";
 		} else {
+			// 失敗時はエラーメッセージをモデルにセットしてニュースフォームを再表示します
 			model.addAttribute("errorMessage", res.getMessage());
 			return showNewsForm(model, model.getAttribute("news"), isNew);
 		}
--- src/main/java/com/example/intern_practice/repository/LatestNewsDao.java
+++ src/main/java/com/example/intern_practice/repository/LatestNewsDao.java
@@ -14,9 +14,11 @@ import com.example.intern_practice.entity.LatestNews;
 @Repository
 public interface LatestNewsDao extends JpaRepository<LatestNews, Integer>{
 
+	// データベースから、表示されたり非表示になったりするデータを取得する
 	@Query(value = "select * from latest_news where reveal = :reveal", nativeQuery = true)
 	public List<LatestNews> searchNews(@Param("reveal") boolean reveal);
 	
+	// キー値に基づいて、データのタイトル、カタログ、サブカタログ、内容の4つの情報を更新する
 	@Transactional
 	@Modifying
 	@Query(value = "update latest_news set catalog = :catalog, subcatalog = :subcatalog, title = :title, "
@@ -24,6 +26,7 @@ public interface LatestNewsDao extends JpaRepository<LatestNews, Integer>{
 	public int updateNews(@Param("serialNumber") Integer serialNumber, @Param("catalog") String catalog, 
 			@Param("subcatalog") String subcatalog, @Param("title") String title, @Param("content") String content);
 	
+	// キー値に基づいて、表示の情報を更新する
 	@Transactional
 	@Modifying
 	@Query(value = "update latest_news set reveal = :reveal where serial_number = :serialNumber", nativeQuery = true)
