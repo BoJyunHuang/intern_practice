fb6b341c228470ea5da16caf3e9a0771ab4b8474
Jyun <taiwanman1212@gmail.com>
2023-09-01 17:59:01 +0800

    only Date to be resolved

--- src/main/resources/static/js/message.js
+++ src/main/resources/static/js/message.js
@@ -10,3 +10,20 @@ function confirmAndSubmit() {
         document.getElementById("addForm").submit();
     }
 }
+
+function deletePersonInfo(employeeNumber) {
+    const formData = new FormData();
+    formData.append('employeeNumber', employeeNumber);
+    fetch('/delete_person_info', {
+        method: 'POST',
+        body: formData
+    }).then(response => {
+        if (response.ok) {
+            location.reload(); 
+        } else {
+            window.alert('Failed to execute');
+        }
+    }).catch(error => {
+        console.error('An error occurred:', error);
+    });
+}
